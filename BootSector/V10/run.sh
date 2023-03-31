66908a9a2e83d25e2997598358b2ccc0ba575212
Lama-Mat9 <lamamat91@gmail.com>
2023-03-31 15:30:38 +0200

    Modified run script

--- BootSector/V10/run.sh
+++ BootSector/V10/run.sh
@@ -1,3 +1,12 @@
+# Compatibility reasons with my home setup
+if command -v qemu-virgil
+then
+	qemu='qemu-virgil'
+elif command -v qemu-system-i386
+then
+	qemu='qemu-system-i386'
+fi
+
 rm -r build/*
 nasm -f bin ./src/main.asm -o ./build/BootSector.bin
-qemu-virgil ./build/BootSector.bin
+$qemu ./build/BootSector.bin

ca68a7c89f99872e6aa98470b784efc625ecd228
Lama-Mat9 <lamamat91@gmail.com>
2023-04-04 18:07:18 +0200

    nuked qemu-system

--- BootSector/latest/run.sh
+++ BootSector/latest/run.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
+rm -r build/*
+nasm -f bin ./src/main.asm -o ./build/BootSector.bin
+
 # Compatibility reasons with my home setup
 if command -v qemu-virgil
 then
-	qemu='qemu-virgil'
-elif command -v qemu-system-i386
-then
-	qemu='qemu-system-i386'
+	qemu-virgil ./build/BootSector.bin
 fi
-
-rm -r build/*
-nasm -f bin ./src/main.asm -o ./build/BootSector.bin
-$qemu ./build/BootSector.bin

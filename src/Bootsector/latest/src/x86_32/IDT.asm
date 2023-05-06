7cb48fcdee3b5e868ca5678a501e0283b1b9ed96
Lama-Mat9 <lamamat91@gmail.com>
2023-05-06 10:36:59 -0700

    Adding interrupt handlers (in progress)

--- src/Bootsector/latest/src/x86_32/IDT.asm
+++ src/Bootsector/latest/src/x86_32/IDT.asm
@@ -1,5 +1,5 @@
 ;
-;	Interrupt descriptor table
+;	Interrupt descriptor table (with their handlers)
 ;
 
 ; First 16 bits: The size of our IDT
@@ -87,3 +87,102 @@ idt_start:
 
 ; No more IDT entries after this label
 idt_end:
+
+
+;       ---- Interrupt handlers definition ----
+
+;
+;   We chose that all interrupts would jump to almost the same handlers.
+;   
+;   There are two kinds of interrupts in this case, the ones which provide 
+;   an error code on the stack (Type 1) and those who don't (Type 0).
+;   
+;   We handle these in a way that there is only one interrupt handler
+;   defined in assembly, which will receive as parameter the interrupt vector
+;   and the error code (error code 0 needs to be passed for no errors).
+;   
+;   This handler will jump to a generic interrupt handler that belongs to our C kernel,
+;   and provides it details about the interrupt it needs to handle.
+
+; Type 0 interrupt handler macro definition
+%macro interrupt_handler_T0 1
+interrupt_handler_%1:
+    push    dword 0                     ; Push 0 as error code
+    push    dword %1                    ; Push the interrupt number
+    jmp     interrupt_handler           ; Jump to the real generic handler
+%endmacro
+
+; Type 1 interrupt handler macro definition
+%macro interrupt_handler_T1 1
+interrupt_handler_%1:                   ; Error code was already pushed by the CPU
+    push    dword %1                    ; Push the interrupt number
+    jmp     interrupt_handler           ; Jump to the real generic handler
+%endmacro
+
+
+; First 32 interrupts are CPU exceptions. We cannot remap them. 
+; It is defined that the interrupts: 8, 10, 11, 12, 13, 14, 17      have error codes (Type 1)
+; All the other interrupts                                          have no error codes (Type 0)
+interrupt_handler_T0 0
+interrupt_handler_T0 1
+interrupt_handler_T0 2
+interrupt_handler_T0 3
+interrupt_handler_T0 4
+interrupt_handler_T0 5
+interrupt_handler_T0 6
+interrupt_handler_T0 7
+interrupt_handler_T1 8
+interrupt_handler_T0 9
+interrupt_handler_T1 10
+interrupt_handler_T1 11
+interrupt_handler_T1 12
+interrupt_handler_T1 13
+interrupt_handler_T1 14
+interrupt_handler_T0 15
+interrupt_handler_T0 16
+interrupt_handler_T1 17
+interrupt_handler_T0 18
+interrupt_handler_T0 19
+interrupt_handler_T0 20
+interrupt_handler_T0 21
+interrupt_handler_T0 22
+interrupt_handler_T0 23
+interrupt_handler_T0 24
+interrupt_handler_T0 25
+interrupt_handler_T0 26
+interrupt_handler_T0 27
+interrupt_handler_T0 28
+interrupt_handler_T0 29
+interrupt_handler_T1 30
+interrupt_handler_T0 31
+
+; Generic interrupt handler. 
+; Passes control and parameters to the kernel.
+interrupt_handler:
+
+; Note:
+
+    ;   Uses iret instruction directly, so macros don't have to.
+    ;   Do not call the handler, it is meant to be jumped to.
+
+; Parameters:
+
+    ;   [ESP]: Interrupt number
+    ;   [ESP+4]: Error code
+
+; ---------------------------------------------------------------------------------------------------
+    ; Save all registers for security because we cannot know
+    ; what the C function will do to them.
+    pusha
+
+    ; Call the generic C interrupt handler function
+    call interrupt_handler
+
+    ; Restore all registers
+    popa
+
+    ; Free stack parameters (two doublewords)
+    add esp, 8
+
+    iret    ; Returns using iret, so that the macros don't have to handle a return
+            ; because it will directly jump back to the code that generated the interrupt.
